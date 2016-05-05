9c07afecbe4bee52671818a74870b4f1d13d1993
leiyz <leiyz@onecloud.cn>
2016-05-05 02:49:00 -0700

    edit gitignore.

--- Crawler/src/main/java/com/pmp/crawler/CSTOProjectCrawler.java
+++ Crawler/src/main/java/com/pmp/crawler/CSTOProjectCrawler.java
@@ -1,11 +1,8 @@
 package com.pmp.crawler;
 
-import cn.edu.hfut.dmic.webcollector.model.CrawlDatum;
 import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Links;
 import cn.edu.hfut.dmic.webcollector.model.Page;
-import cn.edu.hfut.dmic.webcollector.net.HttpRequest;
-import cn.edu.hfut.dmic.webcollector.net.HttpResponse;
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import org.jsoup.select.Elements;
 
@@ -21,16 +18,17 @@ public class CSTOProjectCrawler extends BreadthCrawler {
         super(crawlPath, autoParse);
     }
 
-    @Override
-    public HttpResponse getResponse(CrawlDatum crawlDatum) throws Exception {
-        HttpRequest request = new HttpRequest(crawlDatum);
-        request.setCookie(ZjbLoginCN.getZbjCookie("lyz88119@126.com", "engine"));
-        return request.getResponse();
-    }
+//    @Override
+//    public HttpResponse getResponse(CrawlDatum crawlDatum) throws Exception {
+//        HttpRequest request = new HttpRequest(crawlDatum);
+////        request.setCookie("bdshare_firstime=1462420516150; CSTOID=ns8bie188nkm58cs0hmvqvj854; visite=%2F; login_checked=1; UserName=OOOO00; UserInfo=yuXA6jbFM8Aipvonl8ozIW7ySRd0mRK8Ylchc%252B7jUuRRSKn%252BAmxHeI80XqlLO2SsA2k%252BpvFhf2NpI%252FzLtMJbUDjFoiJx9JzHdwBrCf6Vt3bFTv4XmJOyv69ezvSU%252Bb6D; dc_tos=o6p6cz; dc_session_id=1462440563176; Hm_lvt_67fed6c225de3f90d9f513aed5a91532=1462420028,1462430691; Hm_lpvt_67fed6c225de3f90d9f513aed5a91532=1462440563; __utmt=1; __utma=174166704.1927859358.1462420028.1462436899.1462440563.4; __utmb=174166704.1.10.1462440563; __utmc=174166704; __utmz=174166704.1462420028.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)");
+//        return request.getResponse();
+//    }
 
     @Override
     public void visit(Page page, CrawlDatums crawlDatums) {
-        if (page.getUrl() == "http://www.csto.com/project/list") {
+        System.out.println(page.getUrl());
+        if (page.matchUrl("http://www.csto.com/project/list")) {
             Links pageLinks = page.getLinks("#list_pageshow > a:nth - child(5)");
             if (pageLinks != null && pageLinks.size() == 1) {
                 String[] urlparams = pageLinks.get(0).split("=");
@@ -87,17 +85,17 @@ public class CSTOProjectCrawler extends BreadthCrawler {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int a = 10;
         a--;
         System.out.println(a);
     }
 
-    public static void main1(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         CSTOProjectCrawler crawler = null;
         crawler = new CSTOProjectCrawler("cstoCrawler", true);
         crawler.addSeed("http://www.csto.com/project/list");
         crawler.setThreads(100);
-        crawler.start(2);
+        crawler.start(3);
     }
 }
